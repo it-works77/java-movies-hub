61b29e7c305247b945f2de06401a15f63ed9bce7
Skimmerzzz <skimmer@mail.ru>
2026-06-11 11:58:31 +0300

    Test - getMovies_whenEmpty_returnsEmptyArray

--- src/test/ru/practicum/moviehub/http/MoviesApiTest.java
+++ src/test/ru/practicum/moviehub/http/MoviesApiTest.java
@@ -4,12 +4,37 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.practicum.moviehub.MovieHubApp;
+import ru.practicum.moviehub.store.MoviesStore;
+
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static ru.practicum.moviehub.MovieHubApp.SERVER_BASE_URL;
 
 public class MoviesApiTest {
+    private static MoviesStore store;
+    private static MoviesServer server;
+    private static HttpClient client;
+    private static MoviesStore moviesStore;
+
+    private static final int CONNECTION_TIMEOUT = 5;
 
     @BeforeAll
     static void beforeAll() {
-
+        store = new MoviesStore();
+        server = new MoviesServer(moviesStore, SERVER_BASE_URL);
+        server.start();
+        client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(CONNECTION_TIMEOUT))
+                .build();
     }
 
     @BeforeEach
@@ -19,11 +44,30 @@ public class MoviesApiTest {
 
     @AfterAll
     static void afterAll() {
-
+        if (server != null) {
+            server.stop();
+        }
     }
-
+    // Get movies collection
     @Test
     void getMovies_whenEmpty_returnsEmptyArray() throws Exception {
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create(SERVER_BASE_URL + "/movies")) // !!! Добавьте правильный URI
+                .GET()
+                .build();
 
+        HttpResponse<String> resp =
+                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+        assertEquals(200, resp.statusCode(), "GET /movies должен вернуть 200");
+
+        String contentTypeHeaderValue =
+                resp.headers().firstValue("Content-Type").orElse("");
+        assertEquals("application/json; charset=UTF-8", contentTypeHeaderValue,
+                "Content-Type должен содержать формат данных и кодировку");
+
+        String body = resp.body().trim();
+        assertTrue(body.startsWith("[") && body.endsWith("]"),
+                "Ожидается JSON-массив");
     }
 }
